04367b6a3ecce4f5051045b3f77fd82a29fd5f3e
paolopreve <paolo.prevedelloramirez@studenti.unipd.it>
2025-05-09 12:19:11 +0200

    creato le funzioni per soddisfare i test. Close #3

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -2,3 +2,30 @@
 // ALDO BETTEGA 2101087
 // PAOLO PREVEDELLO RAMIREZ 2111944
 ////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class RomanPrinter {
+    //Funzione che genera la ascii art dato il numero intero
+    public static String print(int num) {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+    //Funzione che genera la ascii art data la stringa del numero romano
+    private static String printAsciiArt(String romanNumber) {
+        StringBuilder asciiArt = new StringBuilder();
+        String[][] asciiLetters = {
+                { " _____ ", "|_   _|", "  | |  ", "  | |  ", " _| |_ ", "|_____|" }
+        };
+        String letters = "I";
+        for (int row = 0; row < 6; row++) {
+            for (char c : romanNumber.toCharArray()) {
+                int index = letters.indexOf(c);
+                asciiArt.append(asciiLetters[index][row]);
+            }
+            if (row < 5) {
+                asciiArt.append("\n");
+            }
+        }
+        return asciiArt.toString();
+    }
+}
