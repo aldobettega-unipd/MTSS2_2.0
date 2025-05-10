954c6a465a9d532c130eec191fb65b2ac087920a
paolopreve <paolo.prevedelloramirez@studenti.unipd.it>
2025-05-10 13:20:41 +0200

    creati nuovi test case per il printerù

--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -19,9 +19,13 @@ public class RomanPrinterTest {
             "3, ' _____  _____  _____ \n|_   _||_   _||_   _|\n  | |    | |    | |  \n  | |    | |    | |  \n _| |_  _| |_  _| |_ \n|_____||_____||_____|'",
             "4, ' _____ __      __\n|_   _|\\ \\    / /\n  | |   \\ \\  / / \n  | |    \\ \\/ /  \n _| |_    \\  /   \n|_____|    \\/    '",
             "5, '__      __\n\\ \\    / /\n \\ \\  / / \n  \\ \\/ /  \n   \\  /   \n    \\/    '",
-            "6, '__      __ _____ \n\\ \\    / /|_   _|\n \\ \\  / /   | |  \n  \\ \\/ /    | |  \n   \\  /    _| |_ \n    \\/    |_____|'"
+            "6, '__      __ _____ \n\\ \\    / /|_   _|\n \\ \\  / /   | |  \n  \\ \\/ /    | |  \n   \\  /    _| |_ \n    \\/    |_____|'",
+            "7, '__      __ _____  _____ \n\\ \\    / /|_   _||_   _|\n \\ \\  / /   | |    | |  \n  \\ \\/ /    | |    | |  \n   \\  /    _| |_  _| |_ \n    \\/    |_____||_____|'",
+            "9, ' _____ __   __\n|_   _|\\ \\ / /\n  | |   \\ V / \n  | |    > <  \n _| |_  / . \\ \n|_____|/_/ \\_\\'",
+            "10, '__   __\n\\ \\ / /\n \\ V / \n  > <  \n / . \\ \n/_/ \\_\\'"
     })
-    //Funzione che testa se la funzione print ritorna la stringa della corrispettiva ascii art del numero romano
+    // Funzione che testa se la funzione print ritorna la stringa della
+    // corrispettiva ascii art del numero romano
     public void testPrint(int input, String expectedOutput) {
         assertEquals(expectedOutput, RomanPrinter.print(input));
     }
