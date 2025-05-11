2fec6941530a38baa57a1a3a1f7bf47364fe162b
paolopreve <paolo.prevedelloramirez@studenti.unipd.it>
2025-05-11 10:48:49 +0200

    aggiornato funzione per soddisfare i test del printer. Close #9

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -6,11 +6,12 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
-    //Funzione che genera la ascii art dato il numero intero
+    // Funzione che genera la ascii art dato il numero intero
     public static String print(int num) {
         return printAsciiArt(IntegerToRoman.convert(num));
     }
-    //Funzione che genera la ascii art data la stringa del numero romano
+
+    // Funzione che genera la ascii art data la stringa del numero romano
     private static String printAsciiArt(String romanNumber) {
         StringBuilder asciiArt = new StringBuilder();
         String[][] asciiLetters = {
@@ -19,8 +20,9 @@ public class RomanPrinter {
                 { "__   __", "\\ \\ / /", " \\ V / ", "  > <  ", " / . \\ ", "/_/ \\_\\" },
                 { " _      ", "| |     ", "| |     ", "| |     ", "| |____ ", "|______|" },
                 { "  _____ ", " / ____|", "| |     ", "| |     ", "| |____ ", " \\_____|" },
+                { " _____  ", "|  __ \\ ", "| |  | |", "| |  | |", "| |__| |", "|_____/ " }
         };
-        String letters = "IVXLC";
+        String letters = "IVXLCD";
         for (int row = 0; row < 6; row++) {
             for (char c : romanNumber.toCharArray()) {
                 int index = letters.indexOf(c);
